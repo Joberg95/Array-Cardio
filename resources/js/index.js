0346bdcc82e4ddd05ec3172bc7615d58a992e05a
nevroje <joakim.berglind@gmail.com>
2019-06-30 21:01:57 +0200

    added sorting algorithms for arrays

--- resources/js/index.js
+++ resources/js/index.js
@@ -56,3 +56,62 @@ const people = [
   "Blair, Tony",
   "Blake, William"
 ];
+
+const fifteen = inventors.filter(
+  inventors => inventor.year >= 1500 && inventor.year < 1600
+);
+console.table(fifteen);
+
+const fullNames = inventors.map(
+  inventor => `${inventor.first} ${inventor.last}`
+);
+console.log(fullNames);
+
+const ordered = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
+console.table(ordered);
+
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+console.log(totalYears);
+
+const oldest = inventors.sort(function(a, b) {
+  const lastInventor = a.passed - a.year;
+  const nextInventor = b.passed - b.year;
+  return lastInventor > nextInventor ? -1 : 1;
+});
+console.log(oldest);
+
+const alpha = people.sort((lastOne, nextOne) => {
+  const [aLast, aFirst] = lastOne.split(", ");
+  const [bLast, bFirst] = nextOne.split(", ");
+  return aLast > bLast ? 1 : -1;
+});
+console.log(alpha);
+
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck",
+  "pogostick"
+];
+
+const transportation = data.reduce(function(obj, item) {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+console.log(transportation);
